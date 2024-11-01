99b982593977dd8fab057ade128ad1f95de96dcf
Rubel Rodriguez <rubel_rodriguez@softshop.com.py>
2024-11-01 13:48:12 -0300

    Se actualiza las propiedades supported_passenger_identity_document_types  y available_actions

--- dist/booking/Orders/OrdersTypes.d.ts
+++ dist/booking/Orders/OrdersTypes.d.ts
@@ -258,7 +258,7 @@ export interface OrderSlice {
     /**
      * Whether this slice can be changed. This can only be true for paid orders.
      */
-    changeable: boolean | null;
+    available_actions: boolean | null;
     /**
      * The conditions associated with this slice, describing the kinds of modifications you can make and any penalties that will apply to those modifications.
      * This condition is applied only to this slice and to all the passengers associated with this order - for information at the order level (e.g. "what happens if I want to change all the slices?") refer to the `conditions` at the top level. If a particular kind of modification is allowed, you may not always be able to take action through the Duffel API. In some cases, you may need to contact the Duffel support team or the airline directly.
